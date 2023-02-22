17393fed002693103a65a3e50dece05d29f75764
Nikita <rogozhin.nickita2018@yandex.ru>
2023-02-22 23:22:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Net.Http;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int speed;
+            int k, m;
+            Console.Write("Введите скорость: ");
+            speed = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(speed);
+            Console.Write("Введите длинну: ");
+            k = Convert.ToInt32(Console.ReadLine());
+            m = 0;
+            while (k < 100)
+            {
+                k++;
+                m++;
+            }
+
+            Console.WriteLine(m);
+            Console.WriteLine(k);
+        }
+    }
+}
---
+++ ConsoleApp1/TextFile1.cs
@@ -0,0 +1,8 @@
+//namespace TextFile1
+//{
+//    internal class Bob
+//    {
+//        private void Main(string[] args)
+//    }
+
+//}
